385cf186117c618101de88a8e8454dbac94d7c27
Jonatas Faria de Oliveira <jonta@pop-os.localdomain>
2023-08-03 23:54:05 -0300

    Cache added

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,8 @@
 import { RouteContext } from "$fresh/server.ts";
 import  NavBar  from "@/islands/NavBar.tsx";
-
 import getTodos from "@/data/todos.tsx"
 
-export default async function MyPage(req: Request, ctx: RouteContext<Project>) {
+export default async function MyPage(req: Request, ctx: RouteContext) {
 
   const value = await getTodos();
       
